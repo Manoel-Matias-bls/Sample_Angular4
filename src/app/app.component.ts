3882d72c0cee15248a92108eab7d0a370245a4c1
Aluno <aluno@academicos.local>
2017-09-05 01:24:06 -0300

    Aula de Angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  curso = "Sistemas de Informação";
+  preco = "800";
+  avaliacao = "AV7 and AVI";
 }
